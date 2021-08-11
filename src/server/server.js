67b112a601225dd30e38b6cfc182842dfa65035a
Daniel Medina <daniel@medinaarmada.com>
2021-08-11 18:15:51 -0400

    Fetch de estado inicial

--- src/server/server.js
+++ src/server/server.js
@@ -79,21 +79,31 @@ const setResponse = (html, preloadedState, manifest) => {
   );
 };
 
-const renderApp = (req, res) => {
+const renderApp = async (req, res) => {
 
   let initialState;
-  const { email, name, id } = req.cookies;
+  const { token, email, name, id } = req.cookies;
 
-  if (id) {
+  try {
+    let movieList = await axios({
+      url: `${process.env.API_URL}/api/movies`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      method: 'get',
+    });
+
+    movieList = movieList.data.data;
+
     initialState = {
       user: {
-        email, name, id
+        id, email, name
       },
       myList: [],
-      trends: [],
-      originals: []
+      trends: movieList.filter(movie => movie.contentRating === 'PG' && movie._id),
+      originals: movieList.filter(movie => movie.contentRating === 'G' && movie._id)
     }
-  } else {
+  }catch(error){
     initialState = {
       user: {},
       myList: [],
@@ -101,6 +111,7 @@ const renderApp = (req, res) => {
       originals: []
     }
   }
+
 
   const store = createStore(reducer, initialState);
   const preloadedState = store.getState();
